46faec4c0113d9c69e694112774a3457b8eadaf9
lalitgiri77480 <59163010+lalitgiri77480@users.noreply.github.com>
2021-07-16 18:23:11 +0530

    Create worker.py

---
+++ worker.py
@@ -0,0 +1,11 @@
+import os 
+os.system("sudo yum install docker -y")
+os.system("sudo systemctl enable docker --now")
+os.system("sudo cp /configure/kubernetes.repo  /etc/yum.repos.d/ ")
+os.system("sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes")
+os.system("sudo systemctl enable --now kubelet") 
+os.system("sudo cp /configure/daemon.json  /etc/docker/")
+os.system("sudo systemctl restart docker ")
+os.system("sudo yum install iproute-tc -y")
+os.system("sudo cp /configure/k8s.conf  /etc/sysctl.d/ ")
+os.system("sudo sysctl --system ")
